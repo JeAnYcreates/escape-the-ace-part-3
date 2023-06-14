22616ce55e36eb2303841bdf295979feb8c62a08
JeAnYcreates <136646682+JeAnYcreates@users.noreply.github.com>
2023-06-14 14:27:13 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+namespace SpriteKind {
+    export const blob = SpriteKind.create()
+    export const boss = SpriteKind.create()
+    export const tiles = SpriteKind.create()
+    export const OVERLAP = SpriteKind.create()
+    export const Void = SpriteKind.create()
+    export const re = SpriteKind.create()
+}
