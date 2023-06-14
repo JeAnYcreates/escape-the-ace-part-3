479fb43fc7cb84bdf4eb8004b69533d6ce9184c6
JeAnYcreates <136646682+JeAnYcreates@users.noreply.github.com>
2023-06-14 14:28:10 -0600

    Restore '6/14/2023, 2:26:53 PM Initialize empty repo'

--- main.ts
+++
@@ -1,8 +0,0 @@
-namespace SpriteKind {
-    export const blob = SpriteKind.create()
-    export const boss = SpriteKind.create()
-    export const tiles = SpriteKind.create()
-    export const OVERLAP = SpriteKind.create()
-    export const Void = SpriteKind.create()
-    export const re = SpriteKind.create()
-}
